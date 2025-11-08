cde80004b0ad1cf33fe8eb1ca3ac6aae8e1025f3
sbx_user1051 <sbx_user1051@169.254.37.191>
2025-11-08 21:22:41 +0000

    Sat Nov 08 21:22:41 UTC 2025

--- test/solutions/HLO/test-hello-solution.js
+++ test/solutions/HLO/test-hello-solution.js
@@ -8,4 +8,9 @@ describe('HLO Challenge: Saying hello', function() {
     it('should return the string "Hello" with the string passed', function() {
         assert.strictEqual(new HelloSolution().hello('Russell'), 'Hello, Russell!')
     })
+
+     it('should reject null or undefined input', function() {
+            assert.throws(() =>new HelloSolution().hello(null), Error);
+            assert.throws(() => new HelloSolution().hello(undefined), Error);
+        });
 })
