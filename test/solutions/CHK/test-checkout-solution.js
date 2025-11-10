ae87a112d008f01f26c82f8ed44795ca430c098c
sbx_user1051 <sbx_user1051@169.254.31.97>
2025-11-10 15:22:16 +0000

    Mon Nov 10 15:22:16 UTC 2025

--- test/solutions/CHK/test-checkout-solution.js
+++ test/solutions/CHK/test-checkout-solution.js
@@ -3,26 +3,26 @@ var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
 const CheckoutSolution = require('../../../lib/solutions/CHK/checkout_solution');
-const PRICES = require('../../../lib/solutions/CHK/inventory/prices')
+const {PRICES} = require('../../../lib/solutions/CHK/inventory/prices')
 
 describe('CHK Challenge: Checkout', function() {
     it('should return zero for empty input', () => {
         assert.strictEqual(new CheckoutSolution().checkout(''), 0)
     })
 
-    it('should return 50 for a single A', () => {
+    it('should return the correct price for a single A', () => {
         assert.strictEqual(new CheckoutSolution().checkout('A'), PRICES['A'])
     })
 
-    it('should return 30 for a single B', () => {
+    it('should return the correct price  for a single B', () => {
         assert.strictEqual(new CheckoutSolution().checkout('B'), PRICES['B'])
     })
 
-    it('should return 20 for a single C', () => {
+    it('should return the correct price  for a single C', () => {
         assert.strictEqual(new CheckoutSolution().checkout('C'), PRICES['C'])
     })
 
-    it('should return 15 for a single D', () => {
+    it('should return the correct price  for a single D', () => {
         assert.strictEqual(new CheckoutSolution().checkout('D'), PRICES['D'])
     })
 
@@ -30,10 +30,13 @@ describe('CHK Challenge: Checkout', function() {
         assert.strictEqual(new CheckoutSolution().checkout(2), -1)
     })
 
-    it('should return the total for multiple items: (ABCD => 115)', () => {
+    it('should return the total for multiple items: ABCD', () => {
         assert.strictEqual(new CheckoutSolution().checkout('ABCD'), 115)
+    })
+
+    it('should return the offer price for three A skus', () => {
+        assert.strictEqual(new CheckoutSolution().checkout('AAA'), 130)
     })
 
     // it('', () => {})
 })
-
