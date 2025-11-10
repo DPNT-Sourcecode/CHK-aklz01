20d32caf350d3a68a2de60bcfbbd91b480b867ed
sbx_user1051 <sbx_user1051@169.254.31.97>
2025-11-10 15:10:16 +0000

    Mon Nov 10 15:10:16 UTC 2025

--- test/solutions/CHK/test-checkout-solution.js
+++ test/solutions/CHK/test-checkout-solution.js
@@ -3,6 +3,7 @@ var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
 const CheckoutSolution = require('../../../lib/solutions/CHK/checkout_solution');
+const PRICES = require('../../../lib/solutions/CHK/inventory/prices')
 
 describe('CHK Challenge: Checkout', function() {
     it('empty input returns zero', () => {
@@ -10,22 +11,23 @@ describe('CHK Challenge: Checkout', function() {
     })
 
     it('single A returns 50', () => {
-        assert.strictEqual(new CheckoutSolution().checkout('A'), 50)
+        assert.strictEqual(new CheckoutSolution().checkout('A'), PRICES['A'])
     })
 
     it('single B returns 30', () => {
-        assert.strictEqual(new CheckoutSolution().checkout('B'), 30)
+        assert.strictEqual(new CheckoutSolution().checkout('B'), PRICES['B'])
     })
 
     it('single C returns 20', () => {
-        assert.strictEqual(new CheckoutSolution().checkout('C'), 20)
+        assert.strictEqual(new CheckoutSolution().checkout('C'), PRICES['C'])
     })
 
     it('single D returns 15', () => {
-        assert.strictEqual(new CheckoutSolution().checkout('D'), 15)
+        assert.strictEqual(new CheckoutSolution().checkout('D'), PRICES['D'])
     })
 
     it('no string input returns -1', () => {
         assert.strictEqual(new CheckoutSolution().checkout(2), -1)
     })
 })
+
