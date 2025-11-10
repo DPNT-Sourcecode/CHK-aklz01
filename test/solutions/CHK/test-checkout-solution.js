af275d8f2b2d06734df70aeb9348a45e168d0552
sbx_user1051 <sbx_user1051@169.254.31.97>
2025-11-10 15:13:16 +0000

    Mon Nov 10 15:13:16 UTC 2025

--- test/solutions/CHK/test-checkout-solution.js
+++ test/solutions/CHK/test-checkout-solution.js
@@ -6,28 +6,34 @@ const CheckoutSolution = require('../../../lib/solutions/CHK/checkout_solution')
 const PRICES = require('../../../lib/solutions/CHK/inventory/prices')
 
 describe('CHK Challenge: Checkout', function() {
-    it('empty input returns zero', () => {
+    it('should return zero for empty input', () => {
         assert.strictEqual(new CheckoutSolution().checkout(''), 0)
     })
 
-    it('single A returns 50', () => {
+    it('should return 50 for a single A', () => {
         assert.strictEqual(new CheckoutSolution().checkout('A'), PRICES['A'])
     })
 
-    it('single B returns 30', () => {
+    it('should return 30 for a single B', () => {
         assert.strictEqual(new CheckoutSolution().checkout('B'), PRICES['B'])
     })
 
-    it('single C returns 20', () => {
+    it('should return 20 for a single C', () => {
         assert.strictEqual(new CheckoutSolution().checkout('C'), PRICES['C'])
     })
 
-    it('single D returns 15', () => {
+    it('should return 15 for a single D', () => {
         assert.strictEqual(new CheckoutSolution().checkout('D'), PRICES['D'])
     })
 
-    it('no string input returns -1', () => {
+    it('should return -1 for an input that is not a valid string', () => {
         assert.strictEqual(new CheckoutSolution().checkout(2), -1)
     })
+
+    it('should return the total for multiple items: (ABCD => 115)', () => {
+        assert.strictEqual(new CheckoutSolution().checkout('ABCD'), 115)
+    })
+
+    // it('', () => {})
 })
 
