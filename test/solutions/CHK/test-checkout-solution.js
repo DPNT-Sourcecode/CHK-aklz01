771acf4357b447d5c39cca1b8e97283ccede2eff
sbx_user1051 <sbx_user1051@169.254.46.35>
2025-11-11 10:35:11 +0000

    Tue Nov 11 10:35:11 UTC 2025

--- test/solutions/CHK/test-checkout-solution.js
+++ test/solutions/CHK/test-checkout-solution.js
@@ -110,4 +110,3 @@ describe('CHK Challenge: checkout(string) -> integer', function () {
         })
     })
 })
-
--- test/solutions/CHK/test-offers-engine.js
+++ test/solutions/CHK/test-offers-engine.js
@@ -4,3 +4,16 @@ var it = mocha.it
 var assert = require('assert');
 const OffersEngine = require('../../../lib/solutions/CHK/offers_engine');
 const { GET_ONE_FREE_OFFERS, MULTI_BUY_OFFERS, PRICES } = require('../../../lib/solutions/CHK/config')
+
+describe('CHK Challenge: OffersEngine.applyOffers(basket) -> {discount: number, remaining: Object}', function () {
+
+    describe('Multi-Buy Offers', () => {
+        it('should return zero discount and same basket when no offers are applied', () => {
+            const offersEngine = new OffersEngine([], { PRICES });
+            const basket = { 'A': 1, 'B': 1 };
+            const { discount, remaining } = offersEngine.applyOffers(basket);
+            assert.strictEqual(discount, 0);
+            assert.deepStrictEqual(remaining, basket);
+        })
+    })
+})
