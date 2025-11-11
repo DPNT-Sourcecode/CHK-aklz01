e4a597ccff9fe43dda27505ad551e0f06fc4cb9c
sbx_user1051 <sbx_user1051@169.254.46.35>
2025-11-11 10:47:11 +0000

    Tue Nov 11 10:47:11 UTC 2025

--- test/solutions/CHK/test-offers-engine.js
+++ test/solutions/CHK/test-offers-engine.js
@@ -2,14 +2,14 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const { OffersEngine } = require('../../../lib/solutions/CHK/offers_engine');
+const { OffersEngine, makeMultiBuyHandler } = require('../../../lib/solutions/CHK/offers_engine');
 const { GET_ONE_FREE_OFFERS, MULTI_BUY_OFFERS, PRICES } = require('../../../lib/solutions/CHK/config')
 
 describe('CHK Challenge: OffersEngine.applyOffers(basket) -> {discount: number, remaining: Object}', function () {
 
     describe('Multi-Buy Offers', () => {
         it('should return zero discount and same basket when no offers are applied', () => {
-            const offersEngine = new OffersEngine([], { PRICES });
+            const offersEngine = new OffersEngine([makeMultiBuyHandler(MULTI_BUY_OFFERS)], { PRICES });
             const basket = { 'A': 1, 'B': 1 };
             const { discount, remaining } = offersEngine.applyOffers(basket);
             assert.strictEqual(discount, 0);
@@ -17,7 +17,7 @@ describe('CHK Challenge: OffersEngine.applyOffers(basket) -> {discount: number, 
         })
 
         it('should apply 3A for 130 offer', () => {
-            const offersEngine = new OffersEngine([], { PRICES });
+            const offersEngine = new OffersEngine([makeMultiBuyHandler(MULTI_BUY_OFFERS)], { PRICES });
             const basket = { 'A': 3 };
             const { discount, remaining } = offersEngine.applyOffers(basket);
             // Unit price: 3 * 50 = 150, offer price = 130, discount = 20
