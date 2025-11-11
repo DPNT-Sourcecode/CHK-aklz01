4116c996316e281e4404175b77f236ae7b2a5dba
sbx_user1051 <sbx_user1051@169.254.46.35>
2025-11-11 10:17:11 +0000

    Tue Nov 11 10:17:11 UTC 2025

--- test/solutions/CHK/test-checkout-solution.js
+++ test/solutions/CHK/test-checkout-solution.js
@@ -3,7 +3,7 @@ var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
 const CheckoutSolution = require('../../../lib/solutions/CHK/checkout_solution');
-const { PRICES, MULTI_BUY_OFFERS } = require('../../../lib/solutions/CHK/config/prices')
+const { PRICES, MULTI_BUY_OFFERS } = require('../../../lib/solutions/CHK/config')
 
 describe('CHK Challenge: checkout(string) -> integer', function () {
 
@@ -110,3 +110,4 @@ describe('CHK Challenge: checkout(string) -> integer', function () {
         })
     })
 })
+
